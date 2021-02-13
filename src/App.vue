4348c8bdffb33b4fea7beb59d5c8f454924a3e3d
relara <relvira_7232@stu.southtexascollege.edu>
2021-02-12 19:04:19 -0600

    My scafold of all things

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,18 @@
 <template>
 <div class="min-h-screen flex flex-col justify-center items-center">
-<header class="flex flex-col w-full container">
-  <h1 class="text-6xl font-light text-grey-800 text-center">My To Do App!</h1>
-  <input class="text-3xl py-2 px-4 rounded-xl w-full mt-8 text-gray-800" type="text" placeholder="New Todo" v-model="newTodo" @change="addTodo"/>
-</header>
-<main class="container w-full mt-8 space-y-8">
-  <section class="space-y-4" v-if="pendingTodos.length > 0">
-    <h3 class="text-3xl font-thin text-green-800">Pending Items: {{pendingTodos.length}}</h3>
-  <ul class="space-y-4">
-    <li v-for="todo in pendingTodos" :key="todo.id" class="bg-white rounded-xl text-2xl py-2 px-4 font-light text-green-800 text-center hover:text-white hover:bg-green-800 cursor-pointer transition-colors" @click="changeStatus(todo.id)">{{todo.text}}</li>
-  </ul>
-  </section>
-  <section class="space-y-4" v-if="completedTodos.length">
-    <h3 class="text-3xl font-thin text-red-800">Completed Items: {{completedTodos.length}}</h3>
-  <ul class="space-y-4">
-    <li v-for="todo in completedTodos" :key="todo.id" class="bg-white rounded-xl text-2xl py-2 px-4 font-light text-red-800 text-center hover:text-white hover:bg-red-800 cursor-pointer transition-colors" @click="changeStatus(todo.id)">{{todo.text}}</li>
-  </ul>
-  </section>
-</main> 
+<Header />
+<Main>
+  <ListTodos color="green" title="Pending Items" :todos="pendingTodos"/>
+  <ListTodos color="red" title="Completed Items" :todos="completedTodos"/>
+</Main>
 </div>
 </template>
-
 <script setup>
-import {ref, computed} from 'vue'
-
-const newTodo = ref('')
-
-const todos = ref([])
-
-const pendingTodos = computed(() =>
-todos.value.filter(todo => todo.status == 'pending'),
-)
-const completedTodos = computed(() =>
-todos.value.filter(todo => todo.status == 'done'),
-)
-const changeStatus = id => {
-  todos.value.map(todo => {
-    if(todo.id == id) {
-      todo.status = todo.status == 'pending' ? 'done' : 'pending'
-    }
-  })
-}
-
-const addTodo = () => {
-  if(newTodo.value.length > 0){
-    todos.value.push({
-      id: todos.value.length,
-      text: newTodo.value,
-      status:'pending',
-    })
-    newTodo.value = ''
-  }
-}
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import ListTodos from './components/ListTodos.vue'
+import { 
+  pendingTodos, 
+  completedTodos,  
+  } from './helpers/useTodos'
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="flex flex-col w-full container">
+    <Title msg="My To Do App"/>
+    <NewTodo />
+    </header>
+</template>
+
+<script setup>
+import Title from './Title.vue'
+import NewTodo from './NewTodo.vue'
+</script>
